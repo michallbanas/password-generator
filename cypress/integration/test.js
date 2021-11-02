05d7deaf7c75829f90386d171063f58acff22370
michallbanas <banas.michal@icloud.com>
2021-11-02 15:23:04 +0100

    cypress integration

---
+++ cypress/integration/test.js
@@ -0,0 +1,37 @@
+/// <reference types="Cypress" />
+
+beforeEach(function(){
+    cy.visit("index.html") 
+}) // pred každým testom, ak chcem iba pred prvým testom tak stačí iba before
+
+describe("Test Password Generator", () => {
+    it("Či vypíše heslo", () => {
+        cy.get("input#lenght")
+            .clear()
+            .type(24) 
+        cy.get("button#btn")
+            .should("be.visible")
+            .should("have.text", "Generovať heslo")
+            .click()
+        cy.get("input#result")
+            .should("not.have.value","")
+    })
+    it("Či vypíše heslo, ak je číslo <12", () => {
+        cy.get("input#lenght")
+            .clear()
+        cy.get("button#btn")
+            .click()
+        cy.get("input#result")
+            .should("have.value","")
+    })
+    it("Či skopíruje heslo", () => {
+        cy.get("input#lenght")
+        cy.get("button#btn")
+            .click()
+        cy.get("#icon")
+            .click()
+        cy.get("input#result")
+            .should("have.value", "Skopírované")
+            .clear()
+    })
+})
